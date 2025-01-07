50419e2124a04a6f2ec70544648b1774a04af48e
Ragu-35 <vraguraman35@gmail.com>
2025-01-07 07:34:00 +0530

    Add files via upload
    week 1 day 2 - Homework

---
+++ Conditionalstatement.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class Conditionalstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for (int i=1;i<=10;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println("The odd number is " +i);
+			}
+			
+			/* else
+			{
+				System.out.println(" the even number is" + i);
+			} */
+		}
+	
+}
+
+}
---
+++ Firefox.java
@@ -0,0 +1,37 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//data types class
+		/* int regno = 8584;
+		String company ="honda";
+		char fuel ='P';
+		double torque = 15.123456789;
+		boolean isbike =true;
+		float weight = 500.45695667858585857f;
+		long phone=8935506565l;
+		
+		System.out.println("regno is "+ regno);
+		System.out.println("Brand name is " +company);
+		System.out.println("Fuel is " + fuel);
+		System.out.println("weight is " + weight);
+		System.out.println("phone number is " +phone);
+		System.out.println("is bike " +isbike); */
+		
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println("browserVersion is "+ browserVersion);
+		System.out.println("browserName is "+ browserName);
+		System.out.println("releaseYear is "+ releaseYear);
+		System.out.println("browserLogo is "+ browserLogo);
+		System.out.println("isVisible is "+ isVisible);
+		
+
+	}
+
+}
